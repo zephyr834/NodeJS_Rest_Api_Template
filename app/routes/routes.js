ecc87ce3fcbd0fc51c9dcdb5a563244aaa245193
Cory Eighan <zephyr834@gmail.com>
2019-07-22 21:29:56 -0700

    Got insert to work correctly from a post request.

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,13 @@
 module.exports = function(app, db) {
   app.post('/notes', (req, res) => {
-    console.log(req.body)
-    res.send('Hello World')
-  })
+    const note = { text: req.body.body, title: req.body.title };
+  
+    db.db().collection('notes').insert(note, (err, result) => {
+      if(err) {
+        res.send({ 'error': 'An error has occured:' + err});
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
 }
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db');
 
 const app = express();
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('./app/routes')(app, {});
-app.listen(port, () => {
-  console.log("Live on port: " + port);
+MongoClient.connect(db.url, { useNewUrlParser: true }, (err, database) => {
+  if(err) return console.log(err);
+
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log("Live on port: " + port);
+  })
 })
