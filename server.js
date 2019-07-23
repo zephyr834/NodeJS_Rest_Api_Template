4ccba8505762fd014bd85742a0a6f4167a087123
Cory Eighan <zephyr834@gmail.com>
2019-07-22 20:18:41 -0700

    Setup basic routes api.

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 
 const app = express();
-
 const port = 8000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./app/routes')(app, {});
 app.listen(port, () => {
   console.log("Live on port: " + port);
 })
