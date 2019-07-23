df5d88695f43b28843bb9746af8c6a9c99b8e386
Cory Eighan <zephyr834@gmail.com>
2019-07-22 19:07:38 -0700

    Setup dev server.

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = 8000;
+
+app.listen(port, () => {
+  console.log("Live on port: " + port);
+})
