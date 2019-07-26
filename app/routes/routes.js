969fb6bc54b314bcf3eb7c98db93f52ccd0680f7
Cory Eighan <zephyr834@gmail.com>
2019-07-26 10:11:16 -0700

    Added GET request for notes.

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,4 +1,18 @@
+var ObjectId = require('mongodb').ObjectId;
+
 module.exports = function(app, db) {
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = {'_id': new ObjectId(id) };
+    db.db().collection('notes').findOne(details, (err, item) => {
+      if(err) {
+        res.send({ 'error': 'An error has occured:' + err });
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
   
