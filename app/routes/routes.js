a4edf6cae637742a14a91dbdb150f83916db1781
Cory Eighan <zephyr834@gmail.com>
2019-07-26 09:54:42 -0700

    Updated deprecated function for insert for POST note.

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,7 +2,7 @@ module.exports = function(app, db) {
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
   
-    db.db().collection('notes').insert(note, (err, result) => {
+    db.db().collection('notes').insertOne(note, (err, result) => {
       if(err) {
         res.send({ 'error': 'An error has occured:' + err});
       } else {
